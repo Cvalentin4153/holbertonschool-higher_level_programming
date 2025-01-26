8e4ca23c7bf4817da474c98addfe5c1a59c9e138
cvalentin4153 <cvalentin4153@outlook.com>
2025-01-26 12:46:02 -0400

    Task 12 fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None:
+    if roman_string == None or not isinstance(roman_string, str):
         return 0
     else:
         roman_values = {
