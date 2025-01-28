b4f38b911268046bff7a1927e05aff805ef12ee5
cvalentin4153 <cvalentin4153@outlook.com>
2025-01-28 11:50:41 -0400

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+class Square:
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new sqaure.
+
+        Args:
+            size: the size of the square.
+        """
+        self.__size = size
