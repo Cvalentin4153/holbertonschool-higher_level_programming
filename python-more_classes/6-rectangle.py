f6bdd118944e85c2675f4f830bb67b1eb57764fe
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-01 16:56:48 -0400

    Task 6 pycodestyle fix

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,10 +6,12 @@ class Rectangle:
     """Represents a rectangle."""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         return self.__width
