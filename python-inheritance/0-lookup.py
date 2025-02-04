a65b62e6a887abe1ef1d0fef7df2aaca0ecd676c
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-04 10:48:17 -0400

    Task 0 fix/Task 1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Defines a function named lookup.
+"""
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    Args: 
+        obj: object
+    Returns:
+        list
+    """
     return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines a class MyList.
+"""
+class MyList(list):
+    """
+    MyList inherits from list.
+    Args:
+        list
+    """
+    def print_sorted(self):
+        """
+        Prints sorted list.
+        """
+        print(sorted(self))
