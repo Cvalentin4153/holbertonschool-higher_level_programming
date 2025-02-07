549d634c2a60f472bfb26f855cb105e9c6a56ee6
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-07 11:21:25 -0400

    Task 8 fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,49 +1,5 @@
 #!/usr/bin/python3
-"""Defines a class BaseGeometry"""
-
-
-class BaseGeometry():
-    """
-    A base class for geometric operations.
-
-    This class provides a foundation for geometric calculations
-    and includes methods for validating integer values. It serves
-    as a blueprint for other geometry-related classes.
-
-    Methods:
-        area(self):
-            Raises an Exception indicating that the method is not implemented.
-
-        integer_validator(self, name, value):
-            Validates that `value` is an integer greater than 0.
-    """
-
-    def area(self):
-        """
-        Calculate the area.
-
-        Raises:
-            Exception: If the area method is not implemented.
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates if a given value is an integer greater than 0.
-
-        Args:
-            name: String with the name of the value to validate.
-            value: Value to validate.
-
-        Raises:
-            TypeError: if value is not an integer.
-            ValueError: if value is not greater than 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__ ('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -63,7 +19,7 @@ class Rectangle(BaseGeometry):
             ValueError: if either width or height is not greater than 0.
         """
 
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
