f932089288f66b548b6aaeacde5904a87a690491
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-09 17:55:18 -0400

    Task 4

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+class Fish:
+    def swim(self):
+        return ("The fish is swimming")
+
+    def habitat(self):
+        return ("The fish lives in water")
+
+
+class Bird:
+    def fly(self):
+        return ("The bird is flying")
+
+    def habitat(self):
+        return ("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        return ("The flying fish is soaring")
+
+    def swim(self):
+        return ("The flying fish is swimming")
+
+    def habitat(self):
+        return ("The flying fish lives both in water and the sky!")
+
+
+fish = FlyingFish()
+print(fish.swim())
+print(fish.fly())
+print(fish.habitat())
