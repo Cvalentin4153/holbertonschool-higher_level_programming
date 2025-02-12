11a97b31bb4aed90663abf6ee93580f745f9d8d6
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-12 12:37:44 -0400

    Documentation fix 0-3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 import json
+"""Defines a function that returns JSON representation of an object"""
 
 
 def to_json_string(my_obj):
+    """
+    Returns a string representation of a JSON object.
+
+    Args:
+        my_obj (str): A string to be encoded in JSON.
+
+    Returns:
+        str: A JSON encoded string.
+    """
+
     return json.dumps(my_obj)
