31f8d19d0ff6d7069111330835b7f31913871092
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-12 14:44:29 -0400

    Task 7 doc and pycode fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import json
-import sys
-
-
 """Script that adds all arguments to a Python
 from os.path import exists
 list and saves them to a file"""
+
+
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
