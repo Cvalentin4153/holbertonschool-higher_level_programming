37e20ab5fd91af4fbac988bedc913a5a5c20946d
cvalentin4153 <cvalentin4153@outlook.com>
2025-02-12 19:21:23 -0400

    Task 12 fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+"""Defines a Pascal's triangle function."""
+
 
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal's triangle of n.
+
+    Returns an empty list if n is 0 or negative.
+    Otherwise, returns a list of lists where each inner
+    list has one more element
+    than the last, and the first and last elements
+    of each inner list are 1.
+    The remaining elements of each inner list are the
+    sum of the two elements
+    directly above them.
+    """
+    
     if n <= 0:
         return []
 
