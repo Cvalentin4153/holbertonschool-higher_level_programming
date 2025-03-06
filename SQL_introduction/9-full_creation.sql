c260b9a433dd266de83dd63d15ceca4aab7d9a1e
cvalentin4153 <cvalentin4153@outlook.com>
2025-03-06 15:18:48 -0400

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- This script creates a table second_table in database hbtn_0c_0
 -- Create the table only if it does not exist
-CREATE TABLE if NOT EXISTS hbtn_0c_0.second_table(
+CREATE TABLE if NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT);
 
 -- Insert the required records
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
-    (1, "John", 10),
-    (2, "Alex", 3),
-    (3, "Bob", 14),
-    (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
