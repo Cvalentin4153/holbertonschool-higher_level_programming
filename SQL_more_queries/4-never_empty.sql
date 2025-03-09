46ce6b42aeeb38cb42464157291c0c2ff7b49d58
cvalentin4153 <cvalentin4153@outlook.com>
2025-03-09 14:37:06 -0400

    Task 4 fix

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
--- This script creates the table id_not_null
+-- This script creates the table id_not_null in MYSQL server
 CREATE IF NOT EXISTS TABLE id_not_null(id INT DEFAULT 1, name VARCHAR(256));
