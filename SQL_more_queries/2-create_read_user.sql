b73c98342614da52e1314ee67842cfbf88b51cf8
cvalentin4153 <cvalentin4153@outlook.com>
2025-03-09 13:36:44 -0400

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- This script creates the MYSLQ user user_0d_2 and database hbtn_0d_2 and grants SELECT privilege
-CREATE USER IF NOT EXISTS 'user_0d_2'@'loclahost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'loclahost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
