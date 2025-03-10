00b7bb8b69e9872f2cfba47719c44d23a942c67b
cvalentin4153 <cvalentin4153@outlook.com>
2025-03-10 12:19:59 -0400

    Task 1 pycode fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,7 +3,8 @@
 Lists all states from the database hbtn_0e_0_usa starting with 'N'.
 Takes 3 arguments: username, password, and db_name.
 Results are sorted in ascending order by states.id.
-Uses MySQLdb to connect to a MySQL server running on localhost at port 3306.
+Uses MySQLdb to connect to a MySQL server running on
+localhost at port 3306.
 """
 import MySQLdb
 import sys
@@ -23,7 +24,8 @@ if __name__ == "__main__":
     )
     # Create a cursor and execute query.
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE states.name like 'N%' ORDER BY states.id ASC")
+    cursor.execute
+    ("SELECT * FROM states WHERE states.name like 'N%' ORDER BY states.id ASC")
     # Fetch and display results.
     rows = cursor.fetchall()
     for row in rows:
