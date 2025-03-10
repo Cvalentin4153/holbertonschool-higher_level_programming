1f127dfa3739a27afc94391ff51ae17f6c0cfe40
cvalentin4153 <cvalentin4153@outlook.com>
2025-03-10 12:28:38 -0400

    Case sensitive fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     # Create a cursor and execute query.
     cursor = conn.cursor()
     cursor.execute("""SELECT * FROM states
-    WHERE states.name LIKE 'N%'
+    WHERE BINARY name LIKE 'N%'
     ORDER BY states.id ASC""")
     # Fetch and display results.
     rows = cursor.fetchall()
