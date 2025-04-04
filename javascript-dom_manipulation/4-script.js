ec0ce814e6e640b2511d0bcdb9264bda1c81c89c
cvalentin4153 <cvalentin4153@outlook.com>
2025-04-04 13:22:00 -0400

    Task 4 fix

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -3,12 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   
   addItemButton.addEventListener('click', function() {
     const myList = document.querySelector('.my_list');
-    
-    // Create a new li element
     const newListItem = document.createElement('li');
     newListItem.textContent = 'Item';
-    
-    // Add the new li element to the ul with class my_list
     myList.appendChild(newListItem);
   });
 });
